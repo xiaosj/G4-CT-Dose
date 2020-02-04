7efb5bab7fd2be49e720e7437fd10d1b288253b0
Shanjie Xiao <xiaosj@users.noreply.github.com>
2020-02-03 22:02:21 -0800

    change scans per run

--- bsub-call.py
+++ bsub-call.py
@@ -5,6 +5,12 @@ import shutil
 
 global img_file, mat_file
 
+
+def get_input(line):
+    loc = line.find('=')
+    return line[loc+1:].strip()
+
+
 def read_limits():
     try:
         f = open('bsub_limits.inp', 'r')
@@ -19,22 +25,28 @@ def read_limits():
         except:
             raise Exception('Fail to create the default bsub_limits.inp')
 
-    s = f.readline()
-    loc = s.find('=')
-    max_jobs = int(s[loc+1:])
+    max_jobs = int(get_input(f.readline()))
 
-    s = f.readline()
-    loc = s.find('=')
-    value = s[loc+1:].strip().split(':')
+    value = get_input(f.readline()).split(':')
     if len(value) == 1:
         max_wall_time = int(value[0]) * 3600
     elif len(value) == 2:
         max_wall_time = int(value[0]) * 3600 + int(value[1]) * 60
     else:
-        raise Exception('Wring wall time input. Must be HH:mm or HH.')
+        raise Exception('Wrong wall time input. Must be HH:mm or HH.')
     
+    spm = int(get_input(f.readline()))
+
+    value = get_input(f.readline()).split(':')
+    if len(value) == 1:
+        tpj = int(value[0]) * 3600
+    elif len(value) == 2:
+        tpj = int(value[0]) * 3600 + int(value[1]) * 60
+    else:
+        raise Exception('Wrong input of time per job. Must be HH:mm or HH.')
+
     f.close()
-    return max_jobs, max_wall_time
+    return max_jobs, max_wall_time, spm, tpj
 
 
 def unfinish_jobs():
@@ -63,30 +75,34 @@ if __name__ == "__main__":
     with open('scan.ini', 'r') as f:
         lines = f.readlines()
 
-    loc = lines[0].find('=')
-    img_file = lines[0][loc+1:].strip()
+    img_file = get_input(lines[0])
     if not os.path.isfile(img_file):
         raise Exception('Cannot find ' + img_file)
 
-    loc = lines[1].find('=')
-    mat_file = lines[1][loc+1:].strip()
+    mat_file = get_input(lines[1])
     if not os.path.isfile(mat_file):
         raise Exception('Cannot find ' + mat_file)
 
-    loc = lines[5].find('=')
-    value = lines[5][loc+1:].strip().split(' ')
-    xmin, xmax, dx = [int(i) for i in value]
+    fixed_estep = len(get_input(lines[3]).split(' '))
+    random_estep = int(get_input(lines[4]).split(' ')[2])
+    estep = fixed_estep + random_estep
 
-    loc = lines[6].find('=')
-    value = lines[6][loc+1:].strip().split(' ')
+    value = get_input(lines[5]).split(' ')
+    xmin, xmax, dx = [int(i) for i in value]
+    nx = (xmax - xmin) // dx + 1
+
+    value = get_input(lines[6]).split(' ')
     zmin, zmax, dz = [int(i) for i in value]
+    nz = (zmax - zmin) // dz + 1
+
+    max_primary_M = int(get_input(lines[9])) / 1e6
 
     flog.write('Scan in the range X[{:d}, {:d}, {:d}] & Z[{:d}, {:d}, {:d}]\n'.format(xmin, xmax, dx, zmin, zmax, dz))
 
     run_start = time.time()
     flog.write('Starting at {:s}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())))
 
-    max_jobs, max_wall_time = read_limits()
+    max_jobs, max_wall_time, spm, tpj = read_limits()
     task_id = 0
     x = xmin
     z = zmin
@@ -97,34 +113,55 @@ if __name__ == "__main__":
             while os.path.isdir(run_name):
                 task_id += 1
                 run_name = 'run_{:04d}'.format(task_id)
-        
+            
+            timestr = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+            flog.write('    X={:d}, Z={:d} --> {:s}: '.format(x, z, run_name))
+
             os.mkdir(run_name)
             with open(os.path.join(run_name, 'scan.ini'), 'w') as f:
-                lines[5] = 'Beam_X = {:d} {:d} 1\n'.format(x, x)
-                lines[6] = 'Beam_X = {:d} {:d} 1\n'.format(z, z)
+                nspot = int(tpj // (spm * max_primary_M * estep))
+                if nspot <= nx:
+                    lines[6] = 'Beam_Z = {:d} {:d} {:d}\n'.format(z, z, dz)
+
+                    delta_x = dx * nspot
+                    x2 = x + delta_x - 1
+                    if x2 > xmax:  x2 = xmax
+                    lines[5] = 'Beam_X = {:d} {:d} {:d}\n'.format(x, x2, dx)
+
+                    x += delta_x
+                    if x >= xmax:
+                        x = xmin
+                        z += dz
+
+                else:
+                    lines[5] = 'Beam_X = {:d} {:d} {:d}\n'.format(xmin, xmax, dx)
+                    
+                    dz_per_run = nspot // nx
+                    delta_z = dz * dz_per_run
+                    z2 = z + delta_z - 1
+                    if z2 > zmax:  z2 = zmax
+                    lines[6] = 'Beam_Z = {:d} {:d} {:d}\n'.format(z, z2, dz)
+
+                    z += delta_z
+
                 f.writelines(lines)
-            x += dx
-            if x > xmax:
-                x = xmin
-                z += dz
+            
             shutil.copy(img_file, run_name)
             shutil.copy(mat_file, run_name)
-            shutil.copy('dosescan', run_name)
+            # shutil.copy('dosescan', run_name)
             os.chdir(run_name)
 
             # busb command to submit a job
-            # submit = subprocess.check_output('bsub -G rpgrp -W 12:00 {:s}/{:s}/dosescan'.format(cwd, run_name), shell=True)
-
-            timestr = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-            flog.write('    X={:d}, Z={:d} --> {:s}: '.format(x, z, run_name))
-            flog.write('{:s}: {:s}'.format(timestr, submit.decode()))
+            # submit = subprocess.check_output('bsub -G rpgrp -W 12:00 {:s}/dosescan'.format(cwd), shell=True)
+            # flog.write('{:s}: {:s}'.format(timestr, submit.decode()))
+            
             os.chdir('..')
 
         else:
             time.sleep(15)
 
         # refresh the limits to allow run-time changes
-        max_jobs, max_wall_time = read_limits()
+        max_jobs, max_wall_time, spm, tpj = read_limits()
 
 
     flog.write('Finished at {:s}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())))
